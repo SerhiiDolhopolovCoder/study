bc9b9454dff9410c92c84552b67c3fc832e5ff61
serhii dolhopolov <serhiidolhopolovcoder@gmail.com>
2025-04-07 04:03:07 +0300

    Train models

--- ML/Dolhopolov_ML_3/features/feature_builder.py
+++ ML/Dolhopolov_ML_3/features/feature_builder.py
@@ -1,13 +1,22 @@
 from abc import ABC, abstractmethod
 
+
 import pandas as pd
+
 from sklearn.preprocessing import MinMaxScaler
 
 
 class FeatureBuilder(ABC):
-    def __init__(self, df):
+    def __init__(self, df: pd.DataFrame, target_column: str):
         self.df = df
-        
+        if target_column in self.df.columns:
+            self.__target = self.df[target_column]
+        self.df.drop(columns=[target_column], inplace=True, errors='ignore')
+    
+    @property
+    def target(self) -> pd.Series:
+        return self.__target
+            
     @abstractmethod
     def build_by_templete(self, new_df=None) -> pd.DataFrame:
         """
@@ -16,15 +25,7 @@ class FeatureBuilder(ABC):
             new_df: pd.DataFrame
         """
         pass
-    
-    def get_features_target(self, target: str) -> tuple[pd.DataFrame, pd.Series]:
-        """Devide dataframe on features and target.
-        Returns:
-            tuple[pd.DataFrame, pd.Series]: (Features, Target)
-
-        """
-        return (self.df.drop(columns=[target]), self.df[target])
-    
+        
     def _hot_encode(self, column: str, dtype: str = 'int8', drop_first: bool = False) -> 'FeatureBuilder':
         """Encode column by hot-encoding:
 
@@ -36,12 +37,22 @@ class FeatureBuilder(ABC):
         Examples:
             hot_encode('Embarked' (Values=[A, B, C])) -> pd.DataFrame where ('Embarked' -> ('Embarked_A','Embarked_B', 'Embarked_C'))
             hot_encode('Embarked' (Values=[A, B, C]), drop_first=True) -> pd.DataFrame where ('Embarked' -> ('Embarked_B', 'Embarked_C'))
-
         """
 
         self.df = pd.get_dummies(
             self.df, columns=[column], prefix=column, dtype=dtype, drop_first=drop_first)
+        return self
+    
+    def _label_encode(self, column: str, encode_map: dict[str, int]) -> 'FeatureBuilder':
+        """Encode column by label-encoding.
+        !!! If value in not in encode_map, it will be replaced Null.
 
+        Args:
+            column (str): a column for label-encoding
+            encode_map (dict[str, int]): map for encoding with type - mark
+        """
+        self.df[f'{column}_encoded'] = self.df[column].map(encode_map)
+        self.df.drop(columns=[column], inplace=True)
         return self
     
     def _normalize_from_0_to_1(self, column: str) -> 'FeatureBuilder':
@@ -49,8 +60,23 @@ class FeatureBuilder(ABC):
 
         Args:
             column (str): a column for normalization
-
         """
-        self.df[column] = MinMaxScaler().fit_transform(self.df[[column]])
-
+        ## Номралізує тільки там де значення >= 0, від'ємні значення не нормалізую, щоб вони не рахувались як
+        ## нормальне значення, але від'ємне
+        positive_mask = self.df[column] >= 0
+        self.df[column] = self.df[column].astype(float)  # Ensure the column is float
+        self.df.loc[positive_mask, column] = MinMaxScaler().fit_transform(self.df.loc[positive_mask, [column]]) 
         return self
+    
+    #отримую список де тільки об'єкти з високою кореляцією та сумарно отсортировані, спочатку дропаю з дуже великою
+    #сумою, потім по-черзі з меншою
+    def get_high_corelation_features(self, theresold: float = 0.75) -> pd.Series:
+        """Return sum of high corelation (>= theresold) features only where correlation > 0 in descending order.
+    
+        Returns:
+            pd.Series: Features with sum of high corelation, where low correlation are thrown out
+        """
+        corr = self.df.corr()
+        corr_filter = corr[(abs(corr) >= theresold) & (abs(corr) != 1)]
+        corr_df = corr_filter.dropna(how='all').dropna(axis=1, how='all')
+        return corr_df.abs().sum().sort_values(ascending=False)
--- ML/Dolhopolov_ML_3/features/house_price_feature_builder.py
+++ ML/Dolhopolov_ML_3/features/house_price_feature_builder.py
@@ -1,58 +1,36 @@
-import pandas as pd
-from pandas.api.types import is_numeric_dtype
 import numpy as np
 
-from features.feature_builder import FeatureBuilder
+import pandas as pd
+from pandas.api.types import is_numeric_dtype
+
+from . import FeatureBuilder
 
 
 class HousePriceFeatureBuilder(FeatureBuilder):
     def __init__(self, df: pd.DataFrame):
-        super().__init__(df)
+        super().__init__(df, 'SalePrice')
 
     def build_by_templete(self, new_df: pd.DataFrame = None) -> pd.DataFrame:
         if new_df:
-            self.df = new_df
+            self.__init__(new_df)
         return self.drop_id() \
                    .drop_duplicates() \
-                   .devide_miscfeature() \
                    .fill_null_columns() \
                    .encode() \
-                   .add_is_alley_access() \
-                   .add_is_basement() \
-                   .add_is_fireplace() \
-                   .add_is_garage() \
-                   .add_is_pool() \
-                   .add_is_fence() \
                    .normalize() \
+                   .drop_high_correlation_features() \
                    .df
 
     def drop_id(self) -> 'HousePriceFeatureBuilder':
-        self.df.drop(columns=['Id'], inplace=True)
+        self.df.drop(columns=['Id'], inplace=True, errors='ignore')
         return self
 
     def drop_duplicates(self) -> 'HousePriceFeatureBuilder':
         self.df.drop_duplicates(inplace=True)
         return self
 
-
-    ## тут 0 де-яких значень, але може потім датафрейм буде оновлятись та будуть значення
-    def devide_miscfeature(self) -> 'HousePriceFeatureBuilder':
-        self.df['has_any_additional_feature'] = np.where(self.df['MiscFeature'].isnull(), 0, 1)
-        self.df['has_elev'] = np.where(self.df['MiscFeature'] == 'Elev', 1, 0) 
-        self.df['has_second_garage'] = np.where(self.df['MiscFeature'] == 'Gar2', 1, 0) 
-        self.df['has_shed'] = np.where(self.df['MiscFeature'] == 'Shed', 1, 0) 
-        self.df['has_tennis_court'] = np.where(self.df['MiscFeature'] == 'TenC', 1, 0) 
-        self.df['has_something'] = np.where(self.df['MiscFeature'] == 'Othr', 1, 0)
-        self.df.drop(columns=['MiscFeature'], inplace=True)        
-        return self
-    
-    def clear_most_null_columns(self) -> 'HousePriceFeatureBuilder':
-        self.df.drop(columns=['MiscFeature'], inplace=True)
-        return self
-
     def fill_null_columns(self) -> 'HousePriceFeatureBuilder':
         self.df['Alley'] = self.df['Alley'].fillna('NoAlley')
-        self.df['PoolQC'] = self.df['PoolQC'].fillna('NoPool')
         self.df['Fence'] = self.df['Fence'].fillna('NoFence')
         self.df['MasVnrType'] = self.df['MasVnrType'].fillna('NoMasonry')
         self.df['FireplaceQu'] = self.df['FireplaceQu'].fillna('NoFireplace')
@@ -65,68 +43,129 @@ class HousePriceFeatureBuilder(FeatureBuilder):
         median_garageyrblt = self.df['GarageYrBlt'].median()
         self.df['GarageYrBlt'] = self.df['GarageYrBlt'].fillna(median_garageyrblt)
 
-        self.df['BsmtExposure'] = self.df['BsmtExposure'].where(self.df['BsmtExposure'] != 'No', 'NoExposure')
+        
         self.df['BsmtExposure'] = self.df['BsmtExposure'].fillna('NoBasement')
         self.df['BsmtFinType1'] = self.df['BsmtFinType1'].fillna('NoBasement')
         self.df['BsmtFinType2'] = self.df['BsmtFinType2'].fillna('NoBasement')
         self.df['BsmtQual'] = self.df['BsmtQual'].fillna('NoBasement')
         self.df['BsmtCond'] = self.df['BsmtCond'].fillna('NoBasement')
         
-        # одне значення та не дуже важливе
-        median_masvnrarea = pd.to_numeric(self.df['MasVnrArea'], errors='coerce').mean()
-        self.df['MasVnrArea'] = self.df['MasVnrArea'].fillna(median_masvnrarea)
+        # 0, бо немає обліцовки
+        self.df['MasVnrArea'] = self.df['MasVnrArea'].fillna(0)
         
+        # важлива х-ка, але мало різних значень, перероблю в HasMiscFeature для фільрування чи є щось особливе ще
+        self.df['HasAdditionalFeature'] = np.where(self.df['MiscFeature'].isnull(), 1, 0)
+        self.df.drop(columns=['MiscFeature'], inplace=True)
+        
+        # Перетворю в чи є інфа, бо є інші х-ка для показу розмірів
         self.df['LotFrontage_missing'] = self.df['LotFrontage'].isnull().astype('int8')
         self.df.drop(columns=['LotFrontage'], inplace=True)
         
         # одне за найбільшим
         self.df['Electrical'] = self.df['Electrical'].fillna('SBrkr')
+        
+        # дуже важлива х-ка, але мало різних значень, перероблю в HasPool
+        self.df['HasPool'] = np.where(self.df['PoolQC'].isnull(), 1, 0)
+        self.df.drop(columns=['PoolQC'], inplace=True)
         return self
-    
+
     def encode(self) -> 'HousePriceFeatureBuilder':
-        columns = ['Alley', 'PoolQC', 'Fence', 'MSSubClass', 'MSZoning', 'Street', 'LotShape', 
+        columns = ['Alley', 'MSSubClass', 'MSZoning', 'Street', 'LotShape', 
                    'LandContour', 'Utilities', 'LotConfig',
                    'LandSlope', 'Neighborhood', 'Condition1', 'Condition2', 'BldgType', 'HouseStyle',
-                   'RoofStyle', 'RoofMatl', 'Exterior1st', 'Exterior2nd', 'MasVnrType', 
-                   'ExterQual', 'ExterCond', 'Foundation', 'BsmtQual', 'BsmtCond', 'BsmtExposure',
-                   'BsmtFinType1', 'BsmtFinType2', 'Heating', 'HeatingQC', 'CentralAir', 'Electrical',
-                   'KitchenQual', 'Functional', 'FireplaceQu', 'GarageType', 'GarageFinish',
-                   'GarageQual', 'GarageCond', 'PavedDrive', 'SaleType', 'SaleCondition']
-        
+                   'RoofStyle', 'RoofMatl', 'Exterior1st', 'Exterior2nd', 'MasVnrType', 'Foundation',
+                   'Heating', 'CentralAir', 'Electrical', 'Functional', 'GarageType', 
+                   'PavedDrive', 'SaleType', 'SaleCondition']
         
         for column in columns:
             self._hot_encode(column, drop_first=True)
+            
+            
+        for column in ['ExterQual', 'ExterCond', 'BsmtQual', 'BsmtCond', 'HeatingQC', 'KitchenQual',
+                       'FireplaceQu', 'GarageQual', 'GarageCond']:
+            self._label_encode(column, self.get_map_mark())
+        for column in ['BsmtExposure']:
+            self._label_encode(column, self.get_map_mark_range())
+        for column in ['BsmtFinType1', 'BsmtFinType2']:
+            self._label_encode(column, self.get_map_rating())
+        for column in ['GarageFinish']:
+            self._label_encode(column, self.get_map_finished())
+        self._label_encode('Fence', self.get_map_fence_quality())
         
         return self
     
-    def add_is_alley_access(self) -> 'HousePriceFeatureBuilder':
-        self.df['IsAlleyAccess'] = np.where(self.df['Alley'] == 'NoAlley', 0, 1) 
-        return self
-    
-    def add_is_basement(self) -> 'HousePriceFeatureBuilder':
-        self.df['IsBasement'] = np.where(self.df['BsmtQual'] == 'NoBasement', 0, 1) 
-        return self
-    
-    def add_is_fireplace(self) -> 'HousePriceFeatureBuilder':
-        self.df['IsFireplace'] = np.where(self.df['FireplaceQu'] == 'NoFireplace', 0, 1) 
-        return self
-    
-    def add_is_garage(self) -> 'HousePriceFeatureBuilder':
-        self.df['IsGarage'] = np.where(self.df['GarageType'] == 'NoGarage', 0, 1) 
-        return self
-    
-    def add_is_pool(self) -> 'HousePriceFeatureBuilder':
-        self.df['IsPool'] = np.where(self.df['PoolQC'] == 'NoPool', 0, 1) 
-        return self
-    
-    def add_is_fence(self) -> 'HousePriceFeatureBuilder':
-        self.df['IsFence'] = np.where(self.df['Fence'] == 'NoFence', 0, 1) 
-        return self
+    def get_map_mark(self) -> dict[str, int]:
+        return {
+            'NoBasement': -1e3,
+            'NoFireplace': -1e3,
+            'NoPool': -1e3,
+            'NoGarage': -1e3,
+            'No': -1e2,
+            'Po': 0,
+            'Fa': 1,
+            'TA': 2,
+            'Gd': 3,
+            'Ex': 4,
+        }
+        
+    def get_map_mark_range(self) -> dict[str, int]:
+        return {
+            'NoBasement': -1e3,
+            'No': -1e2,
+            'Mn': 0,
+            'Av': 1,
+            'Gd': 2,
+        }
+        
+    def get_map_rating(self) -> dict[str, int]:
+        return {
+            'NoBasement': -1e3,
+            'Unf': -1e2,
+            'LwQ': 0,
+            'Rec': 1,
+            'BLQ': 2,
+            'ALQ': 3,
+            'GLQ': 4,
+        }
+        
+    def get_map_finished(self) -> dict[str, int]:
+        return {
+            'NoGarage': -1e3,
+            'Unf': 0,
+            'RFn': 1,
+            'Fin': 2,
+        }
+        
+    def get_map_fence_quality(self) -> dict[str, int]:
+        return {
+            'NoFence': -1e3,
+            'MnWw': 0,
+            'GdWo': 1,
+            'MnPrv': 2,
+            'GdPrv': 3,
+        }
 
+    # мінусових значень нема(тільки label enc, але там є більше 1)
     def normalize(self) -> 'HousePriceFeatureBuilder':
         for column in self.df.columns:
             if is_numeric_dtype(self.df[column]):
                 max_value = self.df[column].max()
                 if max_value > 1:
                     self._normalize_from_0_to_1(column)
+        return self
+    
+    def drop_high_correlation_features(self) -> 'HousePriceFeatureBuilder':
+        self.df.drop(columns=['BsmtQual_encoded', 'BsmtCond_encoded', 'BsmtFinType2_encoded',
+                               'GarageCond_encoded', 'GarageQual_encoded', 'GarageType_NoGarage',
+                               'SaleType_New', 'BsmtFinType1_encoded', '2ndFlrSF',
+                               'HasPool', 'MSSubClass_190', 'Exterior2nd_VinylSd',
+                               'Exterior2nd_CmentBd', 'Exterior1st_MetalSd', 'MSSubClass_80',
+                               'RoofStyle_Hip', 'MSSubClass_45', 'FireplaceQu_encoded',
+                               'Exterior2nd_HdBoard', 'GarageCars', 'MSZoning_FV',
+                               'Exterior2nd_Wd Sdng', 'GrLivArea', 'TotalBsmtSF',
+                               'MSZoning_RM', 'MasVnrType_NoMasonry', 'Foundation_CBlock',
+                               'Exterior2nd_Stucco', 'MSSubClass_120', 'GarageYrBlt',
+                               'SaleCondition_Partial', 'MSSubClass_60', 'Exterior2nd_Plywood',
+                               'BsmtExposure_encoded', 'KitchenAbvGr', 'Neighborhood_NPkVill', 'MSSubClass_85'
+                              ], inplace=True, errors='ignore')
         return self
